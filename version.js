2977333dc4a84df73f84d05803a6f187cdccb636
luwu7375-droid <luwu7375@gmail.com>
2026-07-13 15:43:31 +0800

    chore: bump frontend build for server tools

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // Single source of truth for build version
 // Update this one constant when releasing a new version
-window.SAVE_PRINCESS_BUILD = "cloudflare-0713-cha-b-reference";
+window.SAVE_PRINCESS_BUILD = "cloudflare-0713-server-tools-v1";
 
 // Note: CSS files in index.html <head> also use version strings.
 // Update manually when CSS changes:
