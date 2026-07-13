ad6ed3c74a37a78dc96d0e41fab03021b0feba9b
luwu7375-droid <luwu7375@gmail.com>
2026-07-13 23:20:35 +0800

    chore: release CedarToy machine binding flow

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 // Single source of truth for build version
 // Update this one constant when releasing a new version
-window.SAVE_PRINCESS_BUILD = "cloudflare-0713-mcp-registry-v2";
+window.SAVE_PRINCESS_BUILD = "cloudflare-0714-cedartoy-machine-v1";
 
 // Note: CSS files in index.html <head> also use version strings.
 // Update manually when CSS changes:
